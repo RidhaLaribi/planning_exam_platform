42fd3795879c1252bc11520cc25111a6f787766b
RidhaLaribi <ridalaribi26@gmail.com>
2026-01-16 16:41:33 +0100

    deployment v9

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,30 @@
-// import { redirect } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
-// export default function Home() {
-//   redirect('/login');
-// }
-// pages/seed.js
-'use client';
-
-import { useEffect, useState } from 'react';
-
-export default function SeedPage() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/seed`)
-      .then(res => res.json())
-      .then(json => setData(json))
-      .catch(err => console.error(err));
-  }, []);
-
-  if (!data) return <div>Loading...</div>;
-
-  return (
-    <div>
-      <h1>Seed Result</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
-  );
+export default function Home() {
+  redirect('/login');
 }
+// pages/seed.js
+// 'use client';
+
+// import { useEffect, useState } from 'react';
+
+// export default function SeedPage() {
+//   const [data, setData] = useState(null);
+
+//   useEffect(() => {
+//     fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/seed`)
+//       .then(res => res.json())
+//       .then(json => setData(json))
+//       .catch(err => console.error(err));
+//   }, []);
+
+//   if (!data) return <div>Loading...</div>;
+
+//   return (
+//     <div>
+//       <h1>Seed Result</h1>
+//       <pre>{JSON.stringify(data, null, 2)}</pre>
+//     </div>
+//   );
+// }
 
